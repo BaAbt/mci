71a8612c2fe7653f7f9a0472547178d006bc10d0
BaAbt <48289444+BaAbt@users.noreply.github.com>
2020-07-09 23:46:53 +0000

    Deploying to gh-pages from  @ 1ef8efe6889fba594ca6502115b2a98fa4890529 🚀

--- build/build.js
+++ build/build.js
@@ -138,10 +138,11 @@ function buildTable(headerCells, rows) {
             addExpandedTableEntry(it);
     });
 }
-function cleanTableBody() {
-    var b = table.tBodies[0];
+function cleanTableBody(t) {
+    if (t === void 0) { t = table; }
+    var b = t.tBodies[0];
     if (b == null)
-        b = table.createTBody();
+        b = t.createTBody();
     var size = b.rows.length;
     for (var i = 0; i < size; i++) {
         b.deleteRow(-1);
@@ -294,11 +295,12 @@ function randomId(length) {
 }
 function checkInputs() {
     var name = document.getElementById("name");
-    var number = document.getElementById("name");
+    var number = document.getElementById("number");
     if ((name.value != "") && (number.value != "")) {
         name.setAttribute("readonly", "readonly");
         number.setAttribute("readonly", "readonly");
         document.getElementById("permission").classList.remove("disabled");
+        document.getElementById("login").setAttribute("disabled", "disabled");
         fillPermissionTable(Math.ceil(Math.random() * 3));
     }
     else if (name.value == "") {
@@ -318,22 +320,53 @@ function fillPermissionTable(rows) {
             switch (j) {
                 case 0:
                     var input = document.createElement("input");
+                    input.classList.add("permissionRadio");
                     input.setAttribute("type", "radio");
                     input.setAttribute("name", "permission");
-                    input.setAttribute("value", i.toString());
+                    input.setAttribute("value", "" + i);
                     input.setAttribute("onclick", "document.getElementById('signature').classList.remove('disabled');");
                     td.appendChild(input);
                     break;
                 case 1:
+                    td.classList.add("transponderID");
                     td.innerHTML = transponderList[i];
                     break;
                 case 2:
+                    td.classList.add("rooms");
                     td.innerHTML = roomList_1.join(", ");
             }
             tr.appendChild(td);
         }
-        document.getElementById("permissionTable");
+        document.getElementById("permissionTable").appendChild(tr);
     }
+}
+function submitLoanForm() {
+    console.log("submited");
+    var name = document.getElementById("name");
+    var number = document.getElementById("number");
+    var permission;
+    for (var i = 0; i < document.getElementsByClassName("permissionRadio").length; i++) {
+        var permissionRadio = document.getElementsByClassName("permissionRadio")[i];
+        if (permissionRadio.checked) {
+            permission = i;
+        }
+    }
+    var a = confirm("Freigabe erteilen?\n\nName: " + name.value +
+        "\nMatrikelnummer: " + number.value +
+        "\nTransponderID: " + document.getElementsByClassName("transponderID")[permission].innerHTML +
+        "\nRäume: " + document.getElementsByClassName("rooms")[permission].innerHTML);
+    if (a)
+        statusTable();
+}
+function resetLoan() {
+    document.getElementById("name").removeAttribute("readonly");
+    document.getElementById("number").removeAttribute("readonly");
+    document.getElementById("permissionTable").innerHTML = "";
+    document.getElementById("permission").classList.add("disabled");
+    document.getElementById("signature").classList.add("disabled");
+    document.getElementById("submitButton").classList.add("disabled");
+    document.getElementById("login").removeAttribute("disabled");
+    document.getElementById("permissionTable").innerHTML = "<tr><th></th><th>TransponderID</th><th>Raum</th></tr>";
 }
 var TrTblID = "statusTable";
 var statusTableID = "statusTable";
@@ -385,6 +418,8 @@ function toggleTable(showTable) {
     else {
         tableView.style.display = "none";
         loanView.style.display = "block";
+        loanView.reset();
+        resetLoan();
     }
 }
 function roomToExpandedDom(r) {
--- src/loan.ts
+++ src/loan.ts
@@ -1,7 +1,7 @@
 function checkInputs()
 {
-let name = document.getElementById("name") as HTMLInputElement;
-    let number = document.getElementById("name") as HTMLInputElement;
+    let name     = document.getElementById("name") as HTMLInputElement;
+    let number     = document.getElementById("number") as HTMLInputElement;
 
     if ((name.value != "") && (number.value != ""))
     {
@@ -9,6 +9,7 @@ let name = document.getElementById("name") as HTMLInputElement;
         number.setAttribute("readonly", "readonly");
 
         document.getElementById("permission").classList.remove("disabled");
+        document.getElementById("login").setAttribute("disabled", "disabled");
 
         fillPermissionTable(Math.ceil(Math.random() * 3));
     }
@@ -24,39 +25,79 @@ let name = document.getElementById("name") as HTMLInputElement;
 
 function fillPermissionTable(rows : Number)
 {
-	let transponderList = randomTransponderList(rows).map(element => ("#" + element.id));
+	    let transponderList = randomTransponderList(rows).map(element => ("#" + element.id));
 
-	for (let i = 0; i < rows; i++)
-	{
-		let tr 			= document.createElement("tr");
-		let roomList 	= randomRoomList(Math.ceil(Math.random() * 4)).map(element => (element.nr + " (" + element.name + ")"));
+    for (let i = 0; i < rows; i++)
+    {
+        let tr             = document.createElement("tr");
+        let roomList     = randomRoomList(Math.ceil(Math.random() * 4)).map(element => (element.nr + " (" + element.name + ")"));
 
-		for (let j = 0; j < 3; j++)
-		{
-			let td = document.createElement("td");
+        for (let j = 0; j < 3; j++)
+        {
+            let td = document.createElement("td");
 
-			switch (j)
-			{
-				case 0:
-					let input = document.createElement("input");
+            switch (j)
+            {
+                case 0:
+                    let input = document.createElement("input");
 
-					input.setAttribute("type", 		"radio");
-					input.setAttribute("name", 		"permission");
-					input.setAttribute("value", 	i.toString());
-					input.setAttribute("onclick", 	"document.getElementById('signature').classList.remove('disabled');");
+                    input.classList.add("permissionRadio");
+                    input.setAttribute("type",         "radio");
+                    input.setAttribute("name",         "permission");
+                    input.setAttribute("value",     "" + i);
+                    input.setAttribute("onclick",     "document.getElementById('signature').classList.remove('disabled');");
 
-					td.appendChild(input);
-					break;
-				case 1:
-					td.innerHTML = transponderList[i];
-					break;
-				case 2:
-					td.innerHTML = roomList.join(", ");
-			}
+                    td.appendChild(input);
+                    break;
+                case 1:
+                    td.classList.add("transponderID");
+                    td.innerHTML = transponderList[i];
+                    break;
+                case 2:
+                    td.classList.add("rooms");
+                    td.innerHTML = roomList.join(", ");
+            }
 
-			tr.appendChild(td);
+            tr.appendChild(td);
+        }
+
+        document.getElementById("permissionTable").appendChild(tr)
+    }
+
+}
+function submitLoanForm() {
+	console.log("submited")
+	let name = document.getElementById("name") as HTMLInputElement;
+	let number = document.getElementById("number") as HTMLInputElement;
+	let permission : number ;
+
+	for (let i = 0; i < document.getElementsByClassName("permissionRadio").length; i++) {
+		let permissionRadio = document.getElementsByClassName("permissionRadio")[i] as HTMLInputElement;
+
+		if (permissionRadio.checked) {
+			permission = i;
 		}
-
-		document.getElementById("permissionTable");
 	}
+
+	let a = confirm("Freigabe erteilen?\n\nName: " + name.value +
+		"\nMatrikelnummer: " + number.value +
+		"\nTransponderID: " + document.getElementsByClassName("transponderID")[permission].innerHTML +
+		"\nRäume: " + document.getElementsByClassName("rooms")[permission].innerHTML);
+	if(a)
+		statusTable()
+
+
+}
+
+function resetLoan()
+{
+	document.getElementById("name").removeAttribute("readonly")
+	document.getElementById("number").removeAttribute("readonly")
+    document.getElementById("permissionTable").innerHTML = "";
+
+    document.getElementById("permission").classList.add("disabled")
+    document.getElementById("signature").classList.add("disabled")
+	document.getElementById("submitButton").classList.add("disabled")
+	document.getElementById("login").removeAttribute("disabled");
+	document.getElementById("permissionTable").innerHTML = "<tr><th></th><th>TransponderID</th><th>Raum</th></tr>";
 }
