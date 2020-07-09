b4448af7dc494b8672f696ef760c34cafdb4c1ce
Regrets <jannik-alx@gmx.de>
2020-07-09 16:22:41 +0200

    Update Student.ts
    added Random room number to Rooms

--- site/src/Student.ts
+++ site/src/Student.ts
@@ -32,5 +32,12 @@ function randomRoomList(): Array<Room>{
 function randomRoom(): Room{
     let roomNames: Array<string> = ["PC-Pool","Labor","Seminarraum","Abstellraum","Hexenkammer","Kammer des Schreckens"]
 
-    return new Room((Math.random()*4+1).toString(),roomNames[Math.floor(Math.random()*roomNames.length)], false)
+    return new Room(randomRoomNumber(),roomNames[Math.floor(Math.random()*roomNames.length)], false)
+}
+
+function randomRoomNumber(): string{
+    let firstDigit: string = (Math.random()*4+1).toString().charAt(0)
+    let secondPart: string = (Math.random()*500+100).toString().split(".")[0]
+    let result = firstDigit+"."+secondPart
+    return result
 }
